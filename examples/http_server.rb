006fe48e4126e1f5f4adbc79c5f6680fae407c51
hugoabonizio <hugo_abonizio@hotmail.com>
2014-09-26 20:42:22 +0000

    fix the http server example

--- examples/http_server.rb
+++ examples/http_server.rb
@@ -2,17 +2,19 @@ require File.dirname(__FILE__) + "/../lib/evented.rb"
 
 server = Server.new('0.0.0.0', 8080)
 server.on(:connect) do |sock|
-  puts "connected #{sock}"
-  sock.gets
-  sock << "HTTP/1.1 200 OK\r\n" +
-          "Content-Type: text/html\r\n" +
-          "Content-Length: #{"<h1>lol</h1>".bytesize}\r\n" +
-          "Connection: close\r\n\r\n"
-  sock << "<h1>lol</h1>"
-  sock.close
-rescue Errno::EPIPE
-  # in case the connection closes before
-  # the ende (Ex: if you're using ApacheBench)
+  begin
+    puts "connected #{sock}"
+    sock.gets
+    sock << "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: text/html\r\n" +
+            "Content-Length: #{"<h1>lol</h1>".bytesize}\r\n" +
+            "Connection: close\r\n\r\n"
+    sock << "<h1>lol</h1>"
+    sock.close
+  rescue Errno::EPIPE
+    # in case the connection closes before
+    # the ende (Ex: if you're using ApacheBench)
+  end
 end
 
 server.start
