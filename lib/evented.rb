8dc7081e9b5c42e3bd17afe3bb71b9cb72df4807
hugoabonizio <hugo_abonizio@hotmail.com>
2014-09-26 16:56:17 +0000

    initial commit

---
+++ lib/evented.rb
@@ -0,0 +1,37 @@
+#Dir[File.dirname(__FILE__) + "/*.rb"].each { |file| require file }
+
+class Evented
+  
+  def initialize
+    @@_callbacks ||= Hash.new { |hash, key| hash[key] = Array.new }
+    @@_streams = []
+  end
+  
+  def callbacks
+    @@_callbacks
+  end
+  
+  def on(event, &block)
+    @@_callbacks[event] << block
+  end
+    
+  def emit(event, *args)
+    @@_callbacks[event].each do |callback|
+      callback.call(*args)
+    end
+  end
+  
+  def start
+    @running = true
+    while @running
+      tick
+    end
+  end
+  
+  def tick
+    @@_streams.each do |stream|
+      stream.handle_read
+      stream.handle_write
+    end
+  end
+end
---
+++ spec/events_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Events emmiter and listen" do
+  it "should add event" do
+    events = Evented.new
+    events.callbacks.size.should eq(0)
+    events.on(:teste) do
+      'foo'
+    end
+    events.callbacks.size.should eq(1)
+  end
+  
+  it "should call callback" do
+    events = Evented.new
+    control = 10
+    events.on(:callback_event) do
+      control += 10
+    end
+    events.emit(:callback_event)
+    control.should eq(20)
+  end
+end
