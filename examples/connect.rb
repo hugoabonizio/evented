1e8d8b002bda0363b41935533f75033ec4c4528c
hugoabonizio <hugo_abonizio@hotmail.com>
2014-09-26 19:06:52 +0000

    add server's events (:accept, :connect)

--- examples/connect.rb
+++ examples/connect.rb
@@ -1,20 +1,5 @@
 require 'socket'
 
-#sock = TCPSocket.new('localhost', 8080)
-#sock.send 'haha', 0
-
-require File.dirname(__FILE__) + "/../lib/evented.rb"
-
-server = Server.new('0.0.0.0', 3002)
-control = 10
-server.on(:connect) do |_|
-  control += 10
-end
-Thread.new { server.start }
-
-socket = TCPSocket.new('localhost', 3002)
-sleep(0.1)
-puts control
-
-server.stop
+sock = TCPSocket.new('localhost', 8080)
+sock.send 'haha', 0
 
