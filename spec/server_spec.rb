2dcca3a62989799047715abd9fb15ae0cc30139e
hugoabonizio <hugo_abonizio@hotmail.com>
2014-09-26 17:38:03 +0000

    better test

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Server do
   it "should call Evented constructor" do
     server = Server.new('0.0.0.0', 3001)
-    server.callbacks.should_not be_nil
+    expect(server.callbacks).to be_a Hash
+    #server.callbacks.should_not be_nil
   end
 end
