79884b8b30eddda7d34479bb0f61368ee6e6e228
hugoabonizio <hugo_abonizio@hotmail.com>
2014-09-27 03:39:22 +0000

    add more tests

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'rspec/core/rake_task'
  
 RSpec::Core::RakeTask.new(:spec) do |t|
-  t.ruby_opts = %w[-w]
+  #t.ruby_opts = %w[-w]
   t.rspec_opts = %w[--color]
 end
--- spec/stream_spec.rb
+++ spec/stream_spec.rb
@@ -15,8 +15,32 @@ describe Stream do
   
   it "should close and self delete" do
     stream = Stream.new(File.new(__FILE__))
-    expect(stream.streams).to include stream
+    expect(stream.streams).to include(stream)
     stream.close
-    expect(stream.streams).not_to include stream
+    expect(stream.streams).not_to include(stream)
   end
+  
+  it "should emit events" do
+    stream = Stream.new(File.new(__FILE__))
+    control = 5
+    stream.on(:test) do
+      control = 55
+    end
+    stream.emit(:test)
+    expect(control).to eql(55)
+  end
+  
+  it "should emit events only internally" do
+    stream = Stream.new(File.new(__FILE__))
+    stream2 = Stream.new(File.new(__FILE__))
+    control = 5
+    stream.on(:test) do
+      control = 55
+    end
+    stream2.emit(:test)
+    expect(control).to eql(5)
+  end
+  
+  # TODO
+  # verify the read and write data
 end
