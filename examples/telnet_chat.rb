1556cfea82d464b721617a2f4e64947497cf21a5
hugoabonizio <hugo_abonizio@hotmail.com>
2014-09-28 20:18:03 +0000

    example of a telnet chat

---
+++ examples/telnet_chat.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + "/../lib/evented.rb"
+
+# It's a very simple example of a telnet chat that
+# broadcasts a message to all clients connected, using
+# the concepts of the evented-server and streams
+
+server = Server.new('0.0.0.0', 9090)
+puts 'Listening on 9090...'
+users = []
+server.on(:accept) do |stream|
+  users << stream
+  stream.on(:data) do |data|
+    users.each { |user| user.send(data) unless user == stream }
+  end
+end
+server.start
